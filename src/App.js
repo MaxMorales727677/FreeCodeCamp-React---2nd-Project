cfa514dbdd161e2e93e712f0123305985ed2fc92
MaxMorales727677 <jesus.morales.pintor@u-erre.mx>
2023-05-19 10:23:56 -0600

    Actualizacion de seguridad

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import freeCodeCampLogo from './images/FreeCodeCamp_logo.svg.png';
 
-function App() {
+function App() { 
   return (
     <div className="App">
       <div className='freecodecamp-logo-contenedor'> 
         <img 
           className='freecodecamp-logo'
           src={freeCodeCampLogo}
-          alt='Logo de freeCodeCamp'/>
-      </div>
+          alt='Logo de freeCodeCamp'/> 
+      </div> 
       <div className='contenedor-principal'> 
         
       </div>
