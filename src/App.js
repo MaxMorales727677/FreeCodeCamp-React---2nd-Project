83e635fbd1efda7ec6c25864c6116a2aad79c021
MaxMorales727677 <jesus.morales.pintor@u-erre.mx>
2023-05-20 13:01:42 -0600

    Actualización de contador

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import './hojas-de-estilo/boton.css';
+import './hojas-de-estilo/contador.css';
 import Boton from './components/Boton';
 import freeCodeCampLogo from './images/FreeCodeCamp_logo.svg.png';
+import Contador from './components/Contador';
 
 function App() { 
 
@@ -21,6 +23,7 @@ function App() {
           alt='Logo de freeCodeCamp'/> 
       </div> 
       <div className='contenedor-principal'> 
+      <Contador numClics='5' />
       <Boton 
         texto='Clic'
         esBotonDeClic={true}
--- src/components/Contador.js
+++ src/components/Contador.js
@@ -7,3 +7,5 @@ function Contador({ numClics }) {
         </div>
     );
 }
+
+export default Contador;
